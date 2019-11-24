a1b541ede67b2feb98416d04916b214451138d79
treuther <tracyreuther@gmail.com>
2019-11-24 04:57:51 -0600

    welcome page production

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  
+  get '/' => 'sessions#welcome'
+  get '/login' => 'sessions#new' #existing user needs a session
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new' #a new user needs to be created before it has a session
+  post '/signup' => 'users#create'
+
   resources :reviews
   resources :products
   resources :chem_groups
-  resources :users
+  resources :users #this gives us the user#create
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
