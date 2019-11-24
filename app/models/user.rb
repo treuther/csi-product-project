1fe7e8312b2692166bebb4765ac5c27e8d46a278
treuther <tracyreuther@gmail.com>
2019-11-24 04:36:07 -0600

    defines product/review relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :products, through: :reviews
+    has_many :reviewed_products, through: :reviews, source: :product #creates the relation to Review (belongs_to product)
     has_many :products #that the admin has created
 end
