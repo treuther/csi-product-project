f61aef3f1934ff2e67ea304ee3e66f5475cf6f56
treuther <tracyreuther@gmail.com>
2019-11-24 05:26:25 -0600

    signup completion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,26 @@ class UsersController < ApplicationController
         @user = User.new #define a new user
     end
 
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            # ADD FLASH MESSAGE HERE
+            render :new
+        end
+    end
+
+    def show
+        @user = User.find_by_id(params[:id])
+        redirect_to '/' if !@user #if user doesn't exist, goes home
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :reviewed_products, through: :reviews, source: :product #creates the relation to Review (belongs_to product)
     has_many :products #that the admin has created
+
+    validates :username, uniqueness: true, presence: true
+    
+    has_secure_password
 end
