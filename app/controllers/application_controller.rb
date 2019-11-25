719f4af033d8816cb3f79ff715562bc74e71c832
treuther <tracyreuther@gmail.com>
2019-11-25 12:55:10 -0600

    working on nav_link partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+    helper_method :current_user
+
     private
 
     def current_user
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+    def render_nav_links
+        if current_user
+            render partial: 'layouts/nav_links'
+        end
+    end
 end
